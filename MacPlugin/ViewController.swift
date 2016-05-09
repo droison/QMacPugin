ed68c24b30225f64cab4945cab645e97e3b8eb9a
柴淞 <chaisong.cn@gmail.com>
2016-05-09 20:54:59 +0800

    Initial Commit

---
+++ MacPlugin/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacPlugin
+//
+//  Created by song on 16/5/9.
+//  Copyright © 2016年 song. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
